4eb707946f7658e2adaa1ce073aa4ec4c9b07f03
vrpo <plesco.victor@gmail.com>
2020-06-22 16:34:20 +0200

    data cleansing

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Usefull Links:
 
-1. Source: https://github.com/carloscerlira/COVIDMX/tree/master/examples
+1. Source: https://github.com/carloscerlira/COVIDMX/tree/master
 2. COVID-19 pandemic in Mexico: https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Mexico
--- src/cleansing.R
+++ src/cleansing.R
@@ -1,10 +1,44 @@
+install.packages("knitr")
+require(knitr)
+
+##
+## population
+##
+
 population <- read.csv("~/TABASCO-MEXCOV-19/data/original/datasets_587393_1092718_population_MX.csv", 
                        header = TRUE)
-View(population)
+# View(population)
+knitr::kable(summary(population))
+# which(is.na(population))
 
-confirmed <- read.csv("~/TABASCO-MEXCOV-19/data/original/datasets_587393_1092718_confirmedraw.csv", 
+# {Estado = State, Poblacion = Population}
+colnames(population) <- c("State", "Population")
+
+# {State, Age} = as.factor
+population[, 1] <- lapply(population[, 1], factor)
+
+# knitr::kable(summary(population))
+
+##
+## confirmedraw
+##
+
+confirmedraw <- read.csv("~/TABASCO-MEXCOV-19/data/original/datasets_587393_1092718_confirmedraw.csv", 
                        header = TRUE) 
-View(confirmed)
+# View(confirmedraw)
+knitr::kable(summary(confirmedraw))
+# which(is.na(confirmedraw))
+
+# {Estado = State, Sexo = Gender, Edad = Age, Fecha.de.Inicio.de.sintomas = Date}
+colnames(confirmedraw) <- c("State", "Gender", "Age", "Date")
+
+# {State, Age} = as.factor
+confirmedraw[, 1:2] <- lapply(confirmedraw[, 1:2], factor)
+
+# {Date} = as.Date(%d/%m/%Y)
+confirmedraw$Date <- as.Date(confirmedraw$Date, "%d/%m/%Y")
+
+# knitr::kable(summary(confirmedraw))
 
 ts_confirmed <- read.csv("~/TABASCO-MEXCOV-19/data/original/datasets_587393_1092718_time_series_confirmed_MX.csv",
                          header = TRUE)
