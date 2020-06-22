6c4dec47062552ebd69082e2c6ae9a3b96fa4000
vrpo <plesco.victor@gmail.com>
2020-06-22 19:48:10 +0200

    Update cleansing

--- src/cleansing/ts_deaths.R
+++ src/cleansing/ts_deaths.R
@@ -1,3 +1,5 @@
+source("~/TABASCO-MEXCOV-19/src/cleansing/ts_confirmed.R")
+
 ts_deaths <- read.csv("~/TABASCO-MEXCOV-19/data/original/datasets_587393_1092718_time_series_deaths_MX.csv",
                       header = TRUE)
 # {Estado = State}
@@ -6,3 +8,6 @@ colnames(ts_deaths)[colnames(ts_deaths) == "Estado"] <- "State"
 colnames(ts_deaths)[-1] <- sub("X*", "", colnames(ts_deaths)[-1])
 colnames(ts_deaths)[-1] <- gsub("[.]", "/", colnames(ts_deaths)[-1])
 names(ts_deaths)[-1] <- as.character(as.Date(colnames(ts_deaths)[-1], "%m/%d/%Y"))
+
+tmp <- ts_confirmed %>% select(setdiff(names(ts_confirmed), names(ts_deaths))); tmp[,] <- 0;
+ts_deaths <- cbind(ts_deaths, tmp); ts_deaths <- ts_deaths[, c(names(ts_confirmed))]; rm(tmp, ts_confirmed);
