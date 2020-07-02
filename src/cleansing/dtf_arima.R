4ed1aa14cfbc346f197b72a9189f0cff854ad580
vrpo <plesco.victor@gmail.com>
2020-07-02 12:54:18 +0200

    Update something

--- src/cleansing/dtf_arima.R
+++ src/cleansing/dtf_arima.R
@@ -5,7 +5,11 @@ dtf.reg <- buffers_ts[, c(1, which(colnames(buffers_ts) %in% states))];
 dtf.reg$dTotal <- rowSums(buffers_ts[, 2:19]);
 dtf.reg$aTotal <- cumsum(dtf.reg$dTotal); 
 
-dtf.reg   <- dtf.reg[which(dtf.reg$Date <= "2020-06-15"),];
-train_set <- dtf.reg[which(dtf.reg$Date <= "2020-04-18" & dtf.reg$Date > "2020-03-07"),];
+dtf.reg   <- dtf.reg[which(dtf.reg$Date > "2020-03-07" & dtf.reg$Date <= "2020-06-15"),];
+dtf.reg$lag_1_Confirmed <- c(0, dtf.reg$dTotal[-100])
+dtf.reg$lag_2_Confirmed <- c(rep(0, 2), dtf.reg$dTotal[-c(99:100)])
+dtf.reg$lag_3_Confirmed <- c(rep(0, 3), dtf.reg$dTotal[-c(98:100)])
+
+train_set <- dtf.reg[which(dtf.reg$Date <= "2020-04-18"),];
 test_set  <- dtf.reg[which(dtf.reg$Date >  "2020-04-18"),];
 rm(buffersraw, dtf.reg, buffers_ts, states);
