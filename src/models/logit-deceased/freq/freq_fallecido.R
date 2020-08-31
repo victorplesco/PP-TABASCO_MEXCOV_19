b4365bd49b0665f62b78d64d57955d3f29592a01
vrpo <plesco.victor@gmail.com>
2020-08-31 15:47:03 +0200

    Change of repo

--- src/models/logit-deceased/freq/freq_fallecido.R
+++ src/models/logit-deceased/freq/freq_fallecido.R
@@ -1,11 +1,5 @@
 source("~/TABASCO-MEXCOV-19/src/packages/install.packages.R")
-source("~/TABASCO-MEXCOV-19/src/cleansing/swabsraw_0718.R")
-    
-swabspos <- as.data.frame(swabsraw %>% filter(RESULTADO == "Positive") %>%
-                                       mutate(TIEMPO_INFECCION = as.numeric(as.Date("2020-07-18") - FECHA_SINTOMAS))); # Time being infected; 
-ind      <- which(swabspos$TIEMPO_INFECCION <= 40); swabspos <- swabspos[-ind,]; # After 40d we estimate recovering;                                                
-swabspos <- as.data.frame(swabspos %>% select(-c(ID_REGISTRO, EMBARAZO, NEUMONIA, OTRO_CASO, ENTIDAD_UM, FECHA_INGRESO, FECHA_SINTOMAS, FECHA_DEF, RESULTADO, TIPO_PACIENTE, UCI, INTUBADO, TIEMPO_INFECCION))); rm(ind, swabsraw);
-swabspos <- na.omit(swabspos); # summary(swabspos);
+source("~/TABASCO-MEXCOV-19/src/cleansing/swabspos_0718.R")
 
 #################################################################################################################################################################################################################################################################################
 ## 5-fold Cross Validation ######################################################################################################################################################################################################################################################
