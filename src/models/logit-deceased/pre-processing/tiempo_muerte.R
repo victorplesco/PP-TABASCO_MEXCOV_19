9f95ff27821c93a4c84197b6f1dc3e8e20d31705
vrpo <plesco.victor@gmail.com>
2020-09-05 12:10:02 +0200

    New plots

--- src/models/logit-deceased/pre-processing/tiempo_muerte.R
+++ src/models/logit-deceased/pre-processing/tiempo_muerte.R
@@ -1,10 +1,6 @@
 source("~/TABASCO-MEXCOV-19/src/packages/install.packages.R");
 source("~/TABASCO-MEXCOV-19/src/cleansing/swabsraw_0718.R");
 
-# defaultW <- getOption("warn");
-# options(warn = -1);
-# options(warn = defaultW);
-
 tmp <- as.data.frame(swabsraw %>% filter(RESULTADO == "Positive"));
 TIEMPO_MUERTE <- as.numeric(tmp$FECHA_DEF - tmp$FECHA_SINTOMAS); TIEMPO_MUERTE <- TIEMPO_MUERTE[-c(which(is.na(TIEMPO_MUERTE)), which(TIEMPO_MUERTE <= 0))]; rm(tmp);
 
@@ -15,9 +11,62 @@ TIEMPO_MUERTE <- as.numeric(tmp$FECHA_DEF - tmp$FECHA_SINTOMAS); TIEMPO_MUERTE <
 descdist(TIEMPO_MUERTE, boot = 100, discrete = FALSE);
 
 # Possible methods: “mle”, “mme”, “qme”, “mge”, “mse”; 
-fit.lognorm <- fitdist(TIEMPO_MUERTE, "lnorm", method = c("mse"));
-fit.weibull <- fitdist(TIEMPO_MUERTE, "weibull", method = c("mse"));
+mle.fit <- fitdist(TIEMPO_MUERTE, "lnorm", method = c("mle")); mle <- c(mle.fit$estimate[1], mle.fit$estimate[2]); 
+mme.fit <- fitdist(TIEMPO_MUERTE, "lnorm", method = c("mme")); mme <- c(mme.fit$estimate[1], mme.fit$estimate[2]); 
+mge.fit <- fitdist(TIEMPO_MUERTE, "lnorm", method = c("mge")); mge <- c(mge.fit$estimate[1], mge.fit$estimate[2]); 
+mse.fit <- fitdist(TIEMPO_MUERTE, "lnorm", method = c("mse")); mse <- c(mse.fit$estimate[1], mse.fit$estimate[2]); 
 
+plot_dtf <- data.frame(x     = sort(unique(TIEMPO_MUERTE)),
+                       CDF   = c(cumsum(as.numeric(table(TIEMPO_MUERTE)))/sum(as.numeric(table(TIEMPO_MUERTE))),
+                                 cumsum(dlnorm(unique(TIEMPO_MUERTE), mle.fit$estimate[1], mle.fit$estimate[2])),
+                                 cumsum(dlnorm(unique(TIEMPO_MUERTE), mme.fit$estimate[1], mme.fit$estimate[2])),
+                                 cumsum(dlnorm(unique(TIEMPO_MUERTE), mge.fit$estimate[1], mge.fit$estimate[2])),
+                                 cumsum(dlnorm(unique(TIEMPO_MUERTE), mse.fit$estimate[1], mse.fit$estimate[2]))),
+                       Label = c(rep("ECDF", length(sort(unique(TIEMPO_MUERTE)))),
+                                 rep("MLE",  length(sort(unique(TIEMPO_MUERTE)))),
+                                 rep("MME",  length(sort(unique(TIEMPO_MUERTE)))),
+                                 rep("MGE",  length(sort(unique(TIEMPO_MUERTE)))),
+                                 rep("MSE",  length(sort(unique(TIEMPO_MUERTE))))));
+
+mse_dtf  <- c(1/length(unique(TIEMPO_MUERTE)) *  
+                sum(abs(cumsum(dlnorm(unique(TIEMPO_MUERTE), mle.fit$estimate[1], mle.fit$estimate[2])) +
+                       -cumsum(as.numeric(table(TIEMPO_MUERTE)))/sum(as.numeric(table(TIEMPO_MUERTE))))),
+              1/length(unique(TIEMPO_MUERTE)) *  
+                sum(abs(cumsum(dlnorm(unique(TIEMPO_MUERTE), mme.fit$estimate[1], mme.fit$estimate[2])) +
+                       -cumsum(as.numeric(table(TIEMPO_MUERTE)))/sum(as.numeric(table(TIEMPO_MUERTE))))),
+              1/length(unique(TIEMPO_MUERTE)) *  
+                sum(abs(cumsum(dlnorm(unique(TIEMPO_MUERTE), mge.fit$estimate[1], mge.fit$estimate[2])) +
+                       -cumsum(as.numeric(table(TIEMPO_MUERTE)))/sum(as.numeric(table(TIEMPO_MUERTE))))),
+              1/length(unique(TIEMPO_MUERTE)) *  
+                sum(abs(cumsum(dlnorm(unique(TIEMPO_MUERTE), mse.fit$estimate[1], mse.fit$estimate[2])) +
+                       -cumsum(as.numeric(table(TIEMPO_MUERTE)))/sum(as.numeric(table(TIEMPO_MUERTE))))))
+
+ggplot() +
+  
+  geom_line(data = plot_dtf, aes(x = x, y = CDF, col = Label),
+            size = 0.75) +
+  
+  geom_text(aes(x = 90, y = 0.30), label = paste0("MLE: ", round(mse_dtf[1], 3)), size = 4) +
+  geom_text(aes(x = 90, y = 0.25), label = paste0("MME: ", round(mse_dtf[2], 3)), size = 4) +
+  geom_text(aes(x = 90, y = 0.20), label = paste0("MGE: ", round(mse_dtf[3], 3)), size = 4) +
+  geom_text(aes(x = 90, y = 0.15), label = paste0("MSE: ", round(mse_dtf[4], 3)), size = 4) +
+  
+  
+  scale_color_manual(values = c("black", "tomato3", "forestgreen", "#009dd0", "#f58f3b")) +
+  
+  # Custom Labels;
+  labs(title = "Cumulative Density Functions of Proposed Distributions for Decease Time",
+       subtitle = "Bottom-Left: MAE between the ECDF and Proposals",
+       x = "Decease Time (in days)",
+       y = "Cumulative Density") +
+  theme_bw(base_size = 17.5, base_family = "Times") +
+  theme(legend.title = element_blank()) + 
+  theme(legend.position = "bottom");
+
+#################################################################################################################################################################################################################################################################################
+## TIEMPO_MUERTE | Final distribution ###########################################################################################################################################################################################################################################
+#################################################################################################################################################################################################################################################################################
+  
 ggplot() +
   
   # TIEMPO_MUERTE;
@@ -27,26 +76,17 @@ ggplot() +
             bins = 50) +
   
   # LOG-NORMAL
-  geom_line(aes(x = qlnorm(seq(0.0000001, 0.9995, length = 10000), fit.lognorm$estimate[1], fit.lognorm$estimate[2]), 
-                y = dlnorm(qlnorm(seq(0.00001, 0.9995, length = 10000), fit.lognorm$estimate[1], fit.lognorm$estimate[2]), 
-                                                                        fit.lognorm$estimate[1], fit.lognorm$estimate[2])),
-            col = "tomato3",
+  geom_line(aes(x = qlnorm(seq(0.0000001, 0.9995, length = 10000), mme.fit$estimate[1], mme.fit$estimate[2]), 
+                y = dlnorm(qlnorm(seq(0.00001, 0.9995, length = 10000), mme.fit$estimate[1], mme.fit$estimate[2]), 
+                                                                        mme.fit$estimate[1], mme.fit$estimate[2])),
+            col = "#009dd0",
             size = 0.75) +
   
-  geom_line(aes(x = rep(qlnorm(0.99, fit.lognorm$estimate[1], fit.lognorm$estimate[2]), 2), y = c(0, 0.065)),
-            size = 1.25,
-            col  = "tomato3") +
-  
-  # WEIBULL
-  geom_line(aes(x = qweibull(seq(0.000000001, 0.9999999, length = 10000), fit.weibull$estimate[1], fit.weibull$estimate[2]), 
-                y = dweibull(qweibull(seq(0.0000001, 0.9999999, length = 10000), fit.weibull$estimate[1], fit.weibull$estimate[2]), 
-                                                                                 fit.weibull$estimate[1], fit.weibull$estimate[2])),
-            col = "dodgerblue3",
-            size = 0.75) +
-  
-  geom_line(aes(x = rep(qweibull(0.99, fit.weibull$estimate[1], fit.weibull$estimate[2]), 2), y = c(0, 0.065)),
-            size = 1.25,
-            col  = "dodgerblue3") +
+  geom_line(aes(x = rep(qlnorm(0.99, mme.fit$estimate[1], mme.fit$estimate[2]), 2), y = c(0, 0.065)),
+            size = 0.75,
+            col  = "#009dd0",
+            linetype = "dashed") +
+  geom_text(aes(x = 44, y = 0.0625), label = "99-th Percentile", size = 4) +
   
   # Custom Labels;
   labs(title = "Histogram of Decease Time from Sars-Cov-19 since first Symptoms",
